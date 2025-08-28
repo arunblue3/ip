60971a84a22398602e171c2215e4539127c99eb0
arunblue3 <arunblue3@gmail.com>
2025-08-29 01:50:56 +0800

    level 3

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -20,30 +20,44 @@ public class Buddy {
     public static void main(String[] args) {
         Buddy buddy = new Buddy();
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
-
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println(buddy.getGreeting());
 
         while (true) {
             String command = scanner.nextLine();
 
-            if (command.equals("bye")) {
+            if (command.trim().equals("bye")) {
                 System.out.println(buddy.barWrap(buddy.ending));
                 break;
             }
 
-            else if (command.equals("list")){
-                String currList = "";
+            else if (command.trim().equals("list")){
+                String currList = "Here are the tasks in your list: \n";
                 for (int i = 0; i < tasks.size(); i++){
                     currList += i+1 + ". " + tasks.get(i) + "\n";
                 }
                 System.out.println(buddy.barWrap(currList.trim()));
             }
 
-            else {
+            else if (command.startsWith("mark")) {
+                String[] parts = command.split(" ");
+                int idx = Integer.parseInt(parts[1]) - 1;
+                Task toUpdate = tasks.get(idx);
+                toUpdate.markAsDone();
+                System.out.println(buddy.barWrap("Nice! I've marked this task as done: \n" + toUpdate.toString()));
+            }
 
-                tasks.add(command);
+            else if (command.startsWith("unmark")) {
+                String[] parts = command.split(" ");
+                int idx = Integer.parseInt(parts[1]) - 1;
+                Task toUpdate = tasks.get(idx);
+                toUpdate.unmark();
+                System.out.println(buddy.barWrap("OK, I've marked this task as not done yet: \n" + toUpdate.toString()));
+            }
+
+            else {
+                tasks.add(new Task(command));
                 System.out.println(buddy.barWrap("added: " + command));
             }
         }
