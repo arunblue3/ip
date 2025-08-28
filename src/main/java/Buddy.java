69c9ac478720d5fb43214b0b34d525057ab58229
arunblue3 <arunblue3@gmail.com>
2025-08-28 21:39:18 +0800

    level 1

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,16 +1,38 @@
+import java.util.Scanner;
+
 public class Buddy {
+
+    public String bar = "____________________________________________________________\n";
+
+    public String greeting = " Hello! I'm Buddy \n" +
+            " What can I do for you?\n";
+    public String ending =
+            " Bye. Hope to see you again soon!";
+
+    public String getGreeting() {
+        return bar + greeting + bar;
+    }
+
+    public String barWrap(String msg) {
+        return bar + msg + "\n" + bar;
+    }
+
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-        String level0 = "____________________________________________________________\n" +
-                " Hello! I'm Buddy \n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println( level0);
+        Buddy buddy = new Buddy();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(buddy.getGreeting());
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(buddy.barWrap(buddy.ending));
+                break;
+            }
+
+            System.out.println(buddy.barWrap(command));
+        }
+        scanner.close();
     }
 }
